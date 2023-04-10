0929646266e884a7ddb47ec8d46864e2b8e779dc
MIGUEL ASTOPILLO <condoria.miguel@gmail.com>
2023-04-10 18:23:16 +0200

    completed To do list

--- AngularEx/src/app/app.component.ts
+++ AngularEx/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularEx';
+  Tasks: Array<string> = [];
+  Task: string = '';
+
+  newTask() {
+    if (this.Task == '') {
+      this.Task = "Valore null non valido"
+    } else {
+      this.Tasks.push(this.Task);
+      this.Task = '';
+    }
+  }
+
+  deleteTask(i : number){
+    this.Tasks.splice(i, 1)
+  }
 }
